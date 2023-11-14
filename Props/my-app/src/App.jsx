0fb3f8f10b509990b52a72a4f77f94f61d25ef34
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-11-14 12:52:13 +0100

    props exercise 1

--- Props/my-app/src/App.jsx
+++ Props/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { Hello } from "./Hello";
 import { Message } from "./Message";
+import { Welcome } from "./Welcome";
 
 export function App() {
     return (
         <div>
             <Hello />
             <Message />
+            <Welcome name="Ugo" />
         </div>
     )
 }
