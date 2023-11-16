1f9ebde1d140c12543cda93d949beb2d0e6f9610
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-11-16 13:25:25 +0100

    exercise conditional render 1

--- Props/my-app/src/Welcome.jsx
+++ Props/my-app/src/Welcome.jsx
@@ -4,7 +4,11 @@ export function Welcome({ name, age }) {
     return (
         <div>
             <p>Welcome, <strong>{name}</strong></p>
-            <Age age= {age}/>
+            {<Age age={age} />}
+            {age > 18 && <Age age={age} />}
+            {age && <Age age={age} />}
+            {(age > 18 && age < 65) && <Age age={age} />}
+            {(age > 18 && age < 65 && name === "John") && <Age age={age} />}
         </div>
     )
 }
