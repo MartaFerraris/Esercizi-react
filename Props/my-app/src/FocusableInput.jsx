8e58453d3b3fa18ceff0a22be39e383ae7a0a977
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-11-21 17:53:46 +0100

    ref 2 exercise complete

--- Props/my-app/src/FocusableInput.jsx
+++ Props/my-app/src/FocusableInput.jsx
@@ -2,10 +2,17 @@ import { useEffect, useRef } from "react"
 
 export function FocusableInput() {
     const inputRef = useRef(null)
+    const mountedRef = useRef(false)
 
     useEffect(() => {
+        if (!mountedRef.current) {
+            mountedRef.current = true
+            console.log('First mounting')
+        } else {
+            console.log('You are mounting again for debugging')
+        }
         inputRef.current?.focus()
-    })
+    }, [])
     return (
         <div>
             <h2>Focus</h2>
