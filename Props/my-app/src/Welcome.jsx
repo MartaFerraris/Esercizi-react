d9fd637ec140afddaf87367e537f239e1282df1b
Marta <marta.ferraris25@gmail.com>
2023-11-23 11:52:27 +0100

    styling components 1 exercise complete

--- Props/my-app/src/Welcome.jsx
+++ Props/my-app/src/Welcome.jsx
@@ -2,7 +2,7 @@ import { Age } from "./Age"
 
 export function Welcome({ name, age }) {
     return (
-        <div>
+        <div className="welcome">
             <p>Welcome, <strong>{name}</strong></p>
             {age > 18 && <Age age={age} />}
             {age < 18 && <p>You are very young!</p>}
