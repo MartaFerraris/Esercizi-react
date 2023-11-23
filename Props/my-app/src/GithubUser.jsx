d4ba652babc8435b5e3498342ab9d746d256ab59
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-11-23 20:02:06 +0100

    data fetching exercise

---
+++ Props/my-app/src/GithubUser.jsx
@@ -0,0 +1,4 @@
+/* Create a GithubUser component that receives a username prop and fetches the data of the corresponding Github user from the Github API.
+The component should render the user's name, login and avatar.
+
+Crea un componente GithubUser che riceva una prop username e recuperi i dati del corrispondente utente di Github dall'API di Github. Il componente dovrebbe renderizzare il nome, il login e l'avatar dell'utente. */
