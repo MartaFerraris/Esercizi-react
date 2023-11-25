abaa35b894615541808f7565f4f8ef4dca08e094
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-11-25 13:53:56 +0100

    custom hooks 4 - exercise complete

--- Props/my-app/src/GithubUser.jsx
+++ Props/my-app/src/GithubUser.jsx
@@ -1,14 +1,21 @@
+import { useEffect } from "react"
 import { useGithubUser } from "./useGithubUser"
 
 export function GithubUser({ username }) {
 
-    const {data} = useGithubUser(username)
+    const {data, error, loading, onFetchUser} = useGithubUser(username)
+
+    useEffect(() => {
+        onFetchUser(username)
+    }, [username])
 
     return (
         <div>
             <h2>Github user</h2>
+            {loading && <div>Loading...</div>}
             {data && <img className="gituser" src={data.avatar_url} alt="profile image" />}
             {data && <div>User name: {data.name}; User login: {data.login}</div>}
+            {!data && error && <div>User not found.</div> }
         </div>
     )
 }
--- Props/my-app/src/useGithubUser.js
+++ Props/my-app/src/useGithubUser.js
@@ -1,18 +1,26 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export function useGithubUser(username) {
     const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
-    useEffect(() => {
+    function fetchGithubUser() {
+        setLoading(true)
         fetch(`https://api.github.com/users/${username}`)
             .then(response => { return response.json() })
             .then(json => {
                 console.log(json)
                 setData(json)
             })
-    }, [username])
+            .catch(error => setError(error))
+            .finally(() => setLoading(false))
+    }
 
     return {
-        data: data
+        data: data,
+        error: error,
+        loading: loading,
+        onFetchUser: fetchGithubUser
     }
 }
