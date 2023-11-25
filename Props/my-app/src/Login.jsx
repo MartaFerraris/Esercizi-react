218d86bc4dfa7f8530749f3722d85dc5a67749d9
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-11-25 13:34:06 +0100

    custom hooks 2 - exercise complete

--- Props/my-app/src/Login.jsx
+++ Props/my-app/src/Login.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
+import { useUser } from "./useUser";
 
 export function Login({ onLogin }) {
 
-    const [data, setData] = useState({
+    /* const [data, setData] = useState({
         username: '',
         password: '',
         checkbox: false
@@ -20,7 +21,7 @@ export function Login({ onLogin }) {
                 [name]: type === 'checkbox' ? checkbox : value,
             }
         })
-    }
+    } */
 
     function handleReset() {
         setData({
@@ -36,15 +37,17 @@ export function Login({ onLogin }) {
         // permette una gestione personalizzata dell'invio del modulo secondo la propria logica. 
     }
 
+    const {data, onInput} = useUser()
+
     return (
         <div>
             <h2>Login</h2>
             <form onSubmit={handleForm}>
                 <label>Username:</label>
-                <input name="username" value={data.username} onChange={handleLogin} />
+                <input name="username" value={data.username} onChange={onInput} />
                 <label>Password:</label>
-                <input name="password" type="password" value={data.password} onChange={handleLogin} />
-                <input name="checkbox" type="checkbox" checked={data.checkbox} onChange={handleLogin} />
+                <input name="password" type="password" value={data.password} onChange={onInput} />
+                {/* <input name="checkbox" type="checkbox" checked={data.checkbox} onChange={handleLogin} /> */}
                 <button type="submit" disabled={!data.username || !data.password} onClick={() => onLogin(data)}>Login</button>
                 <button onClick={handleReset}>Reset</button>
             </form>
