010cdb48963b9fadae3b8335b7fff3b3a6b1ac53
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-11-25 13:40:37 +0100

    custom hooks 3 - exercise complete

--- Props/my-app/src/GithubUser.jsx
+++ Props/my-app/src/GithubUser.jsx
@@ -1,17 +1,8 @@
-import { useEffect, useState } from "react"
+import { useGithubUser } from "./useGithubUser"
 
 export function GithubUser({ username }) {
 
-    const [data, setData] = useState(null)
-
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-            .then(response => { return response.json() })
-            .then(json => {
-                console.log(json)
-                setData(json)
-            })
-    }, [username])
+    const {data} = useGithubUser(username)
 
     return (
         <div>
